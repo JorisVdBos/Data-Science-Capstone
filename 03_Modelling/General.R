51a607d9c998d9d81f18821917e85b17ef2ff965
vandej26 <joris.vandenbossche@infofarm.be>
2016-12-07 10:23:58 +0100

    Updated predict function for partial word prediction

--- 03_Modelling/General.R
+++ 03_Modelling/General.R
@@ -1,7 +1,10 @@
 # Filter an input string to an input into a model. This is basically the last two words, with the same filters used to make the string
 modelInput <- function(string, mode = "model"){
+  # Check input
+  if(length(string) == 0 || string == "") 
+    return(list(word1 = "\n",
+                word2 = "\n"))
   
-  if(length(string) == 0) return(NULL)
   
   # Pre filtering
   string <- gsub(" $", "", string)
@@ -10,10 +13,9 @@ modelInput <- function(string, mode = "model"){
   Encoding(string) <- "UTF-8"
   string <- Corpus(VectorSource(string))
   
-  # Apply corpus filter
+  # Apply corpus filter. This function can be found in the map "load" and 
+  # should be the same filter used to construct the model
   string <- corpusFilter(string)
-  
-  # Extra filtering
   
   # Split the string to return the last 2 words
   if((class(string[[1]]) == "character")[1])
@@ -27,10 +29,27 @@ modelInput <- function(string, mode = "model"){
 
 
 # Prediction function
-predict.FreqModel <- function(model, word1 = NULL, word2 = NULL){
+predict.FreqModel <- function(model, string = NULL){
   
+  # Check if the input ends with a space. If not, we can try to match
+  # the partial word
+  if(length(grep(" $", string)) == 0 && string != ""){
+    predictPart <- TRUE
+    
+    # Get third 3 partial and indices
+    inputWord3Part <- strsplit(string, split = " ")[[1]]
+    inputWord3Part <- inputWord3Part[length(inputWord3Part)]
+    inputWord3Part <- modelInput(inputWord3Part)$word2
+    inputWord3PartIndices <- grep(paste0("^", inputWord3Part), model$wordFreqTable$word)
+    
+    # Remove word from input string
+    string <- gsub(paste0(" ", inputWord3Part, "$"), "", string)
+  } else {
+    predictPart <- FALSE
+  }
+  
+  # Find which indices are off limits
   doNotPredictIndex <- which(model$wordFreqTable$word %in% doNotPredict)
-  
   if("\n" %in% doNotPredict)
     doNotPredictIndex <- c(0, doNotPredictIndex)
   
@@ -38,44 +57,24 @@ predict.FreqModel <- function(model, word1 = NULL, word2 = NULL){
   if(is.null(giveNumberOfPossibilities)) giveNumberOfPossibilities <- 3
   
   # Post filtering
-  word1 <- gsub(" $", "", word1)
-  word1 <- modelInput(word1)$word2
-  word2 <- modelInput(word2)$word1
-  
-  # If word1 is empty, regard the input as the beginning of a sentence:
-  if(is.null(word1)){
-    word1 <- "/n"
-  }
-  
-  # If word2 is empty, check if the input of word1 is a sentence. If so split it up. If not, regard the input as a first word of a sentence:
-  if(is.null(word2)){
-    word1Split <- strsplit(word1, " ")[[1]]
-    if(length(word1Split) == 1){
-      word2 <- word1
-      word1 <- "/n"
-    } else {
-      word1ModelInput <- modelInput(word1)
-      word1 <- word1ModelInput$word1
-      word2 <- word1ModelInput$word2
-    }
-  }
+  word1 <- modelInput(string)$word1
+  word2 <- modelInput(string)$word2
   
   # Finding input word indices
-  if(length(word1) == 0 || word1 == "\n") inputWord1Index <- 0 else
+  if(word1 == "\n") inputWord1Index <- 0 else
     inputWord1Index <- which(model$wordFreqTable$word == word1)
-  
-  if(length(word1) > 1) {
-    print("length(word1) > 1")
-    print(word1)
-  }
-  
   if(length(word2) == 0 || word2 == "\n") inputWord2Index <- 0 else
     inputWord2Index <- which(model$wordFreqTable$word == word2)
   
   # Find solutions in the 3-grams table
   solutions <- model$n3gramsTable[indexWord1 == inputWord1Index & indexWord2 == inputWord2Index]
+  
+  # Filter solutions
+  if(predictPart)
+    solutions <- solutions[indexWord3 %in% inputWord3PartIndices]
+  solutions <- solutions[!(indexWord3 %in% doNotPredictIndex)]
+  
   # Take the first three solutions
-  solutions <- solutions[-which(solutions$indexWord3 %in% doNotPredictIndex),]
   solutions <- data.table(value = solutions$indexWord3[1:giveNumberOfPossibilities])
   
   solutions$source <- rep("n3gramsTable", giveNumberOfPossibilities)
@@ -85,13 +84,17 @@ predict.FreqModel <- function(model, word1 = NULL, word2 = NULL){
   if(findMore > 0){
     sol2gram <- model$n2gramsTable[indexWord1 == inputWord2Index]$indexWord2
     
+    # Filter partial
+    if(predictPart)
+      sol2gram <- intersect(sol2gram, inputWord3PartIndices)
+    
+    k <- 1
     for(i in 1:findMore){
-      i <- as.numeric(i)
-      sol <- sol2gram[i]
+      sol <- sol2gram[k]
       
       # Check if the found solutions has allready been found in the 3-gram before
-      k <- i
-      while((sol %in% solutions$value) || (sol %in% doNotPredictIndex)){
+      while((sol %in% solutions$value) || 
+            (sol %in% doNotPredictIndex)){
         k <- k + 1
         sol <- sol2gram[k]
         if(is.na(sol)) break
@@ -100,29 +103,41 @@ predict.FreqModel <- function(model, word1 = NULL, word2 = NULL){
       # Add 2gram solution to solutions vector
       solutions$value[giveNumberOfPossibilities - findMore + i] <- sol
       solutions$source[giveNumberOfPossibilities - findMore + i] <- "2gramsTable"
+      k <- k + 1
     }
   }
   
   # Find more solutions in the freqency table if necessairy
   findMore <- sum(is.na(solutions$value))
   if(findMore > 0){
-    
+    if(predictPart)
+      solFreq <- inputWord3PartIndices else
+        solFreq <- 1:length(model$wordFreqTable$word)
+      
+    k <- 1
     for(i in 1:findMore){
+      sol <- solFreq[k]
+      
       # Check if the found solutions has allready been found in the 3-gram before
-      k <- as.numeric(i)
-      while((k %in% solutions$value) || (k %in% doNotPredictIndex)){
+      while((sol %in% solutions$value) || 
+            (sol %in% doNotPredictIndex)){
         k <- k + 1
+        sol <- solFreq[k]
+        if(is.na(sol)) break
       }
       
       # Add solution to solutions vector
-      solutions$value[giveNumberOfPossibilities - findMore + i] <- k
+      solutions$value[giveNumberOfPossibilities - findMore + i] <- sol
       solutions$source[giveNumberOfPossibilities - findMore + i] <- "wordFreqencyTable"
+      k <- k + 1
     }
   }
   
   solutions$value <- as.numeric(solutions$value)
   # Replace indices by words
   getWord <- function(index){
+    if(is.na(index))
+      return(NA)
     if(index == 0) return("\n") else
       return(model$wordFreqTable[index, "word", with = FALSE]$word)
   }
